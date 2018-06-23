687ad204ff768bb201448381f01db537dcf82af4
Restry Louis <michael.wei@foxmail.com>
2018-06-23 23:03:01 +0800

    personal site init

---
+++ src/pages/contact.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import HeadlineWithFocus from "../components/HeadlineWithFocus";
+import Obfuscate from "react-obfuscate";
+import { FaEnvelopeO, FaTwitter, FaGithub } from "react-icons/lib/fa/";
+import Helmet from "react-helmet";
+
+export default props => {
+  return (
+    <div className="c-content-box">
+      <Helmet title="Contact me" />
+
+      <HeadlineWithFocus>
+        You can get in touch <span>anytime through</span>
+      </HeadlineWithFocus>
+      <ul className="c-contact-list">
+        <li className="c-contact-list__item">
+          <FaEnvelopeO />
+          <Obfuscate email="restry@gmail.com" />
+        </li>
+        <li className="c-contact-list__item">
+          <FaTwitter />
+          <a
+            href="https://twitter.com/restry"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @restry
+          </a>
+        </li>
+        <li className="c-contact-list__item">
+          <FaGithub />
+          <a
+            href="https://github.com/restry"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            restry
+          </a>
+        </li>
+      </ul>
+      <h2
+        className={`
+        js-action-button-anchor
+        c-content-box__subheadline 
+        c-content-box__subheadline--with-button
+      `}
+      >
+        Do not forget
+      </h2>
+    </div>
+  );
+};
