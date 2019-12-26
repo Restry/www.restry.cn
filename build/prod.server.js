abb40ba2ea8e7ffd018f1184f50fe9b74c862bf8
Restry-MBP-18 <michael.wei@foxmail.com>
2019-12-26 15:32:40 +0800

    test

--- build/prod.server.js
+++ build/prod.server.js
@@ -4,18 +4,27 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-const api = require('./yuque');
-const fallback = require('connect-history-api-fallback')
 
 const router = express.Router();
- 
-router.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../dist/index.html')));
-router.use('/api', api);
- 
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello there this is restry fork from netlify Express.js!</h1>');
+  res.end();
+}); 
+
+router.get('/test', (req, res) => {
+  res.send('test'+new Date());
+  res.end();
+  console.log('test'+new Date())
+});
+
+
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-app.use(fallback());
 
 module.exports = app;
 module.exports.handler = serverless(app);
