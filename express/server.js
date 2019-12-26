c3a6df785f710b92fea14e2ffb82b9642e6f0ba5
Restry-MBP-18 <michael.wei@foxmail.com>
2019-12-26 16:49:14 +0800

    update server

--- express/server.js
+++ express/server.js
@@ -8,8 +8,12 @@ const api = require('./yuque');
 const fallback = require('connect-history-api-fallback')
 
 const router = express.Router();
- 
-router.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../dist/index.html')));
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello there this is restry fork from netlify Express.js!</h1>');
+  res.end();
+}); 
+// router.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../dist/index.html')));
 // router.use('/api', api);
  
 app.use(bodyParser.json());
