cc6e154db163fa3a578c459ac22e58714a22fdea
LewayHackintosh <michael.wei@foxmail.com>
2022-04-23 11:30:49 +0800

    start with env

--- express/server.js
+++ express/server.js
@@ -1,13 +1,13 @@
-'use strict';
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const serverless = require('serverless-http');
+"use strict";
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const serverless = require("serverless-http");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 // const api = require('../build/yuque');
 // const fallback = require('connect-history-api-fallback')
-const SDK = require('@yuque/sdk');
+const SDK = require("@yuque/sdk");
 
 const client = new SDK({
   token: process.env.YQTOKEN,
@@ -21,71 +21,77 @@ const localDB = {};
 // apis
 const { users, groups, repos, docs } = client;
 
-
 const router = express.Router();
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   const { p } = req.query;
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.writeHead(200, { "Content-Type": "text/html" });
   console.log(`query:${JSON.stringify(req.query)}`);
-  res.write(fs.readdirSync(path.resolve(__dirname, decodeURIComponent(p))).join(';'));
+  res.write(
+    fs.readdirSync(path.resolve(__dirname, decodeURIComponent(p))).join(";")
+  );
   // res.sendFile(path.resolve(__dirname, '../dist/index.html'))
   res.end();
 });
 
 const errorHandler = (res) => (err) => {
-  res.json({ ok: false, message: err.message })
-}
+  res.json({ ok: false, message: err.message });
+};
 
-
-router.get('/books', (req, res) => {
+router.get("/books", (req, res) => {
   if (!localDB.books) {
-    repos.list({ user: 'restry' }).then((books) => {
-      localDB.books = books.filter(a => a.public);
-      res.json(localDB.books);
-    }).catch(errorHandler(res));
-    return
+    repos
+      .list({ user: "restry" })
+      .then((books) => {
+        localDB.books = books.filter((a) => a.public);
+        res.json(localDB.books);
+      })
+      .catch(errorHandler(res));
+    return;
   }
 
   res.json(localDB.books);
 });
 
-router.get('/docs/:id', (req, res) => {
+router.get("/docs/:id", (req, res) => {
   const { id } = req.params;
-  const cacheDocId = `docs_${id}`
+  const cacheDocId = `docs_${id}`;
 
   if (!localDB[cacheDocId]) {
-    docs.list({ namespace: `restry/${id}` }).then((doc) => {
-      localDB[cacheDocId] = doc;
-      res.json(localDB[cacheDocId]);
-    }).catch(errorHandler(res));;
-    return
+    docs
+      .list({ namespace: `restry/${id}` })
+      .then((doc) => {
+        localDB[cacheDocId] = doc;
+        res.json(localDB[cacheDocId]);
+      })
+      .catch(errorHandler(res));
+    return;
   }
   res.json(localDB[cacheDocId]);
-})
+});
 
-router.get('/doc/:bookId/:id', (req, res) => {
+router.get("/doc/:bookId/:id", (req, res) => {
   const { id, bookId } = req.params;
-  const cacheDocId = `doc_${id}`
+  const cacheDocId = `doc_${id}`;
 
   if (!localDB[cacheDocId]) {
-    docs.get({ namespace: `restry/${bookId}`, slug: id }).then((doc) => {
-      localDB[cacheDocId] = doc;
-      res.json(localDB[cacheDocId]);
-    }).catch(errorHandler(res));;
-    return
+    docs
+      .get({ namespace: `restry/${bookId}`, slug: id })
+      .then((doc) => {
+        localDB[cacheDocId] = doc;
+        res.json(localDB[cacheDocId]);
+      })
+      .catch(errorHandler(res));
+    return;
   }
   res.json(localDB[cacheDocId]);
-})
-
-
-
+});
 
 // router.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../dist/index.html')));
 // router.use('/api', api);
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use("/.netlify/functions/server", router); // path must route to lambda
+app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 // app.use(fallback());
 
 module.exports = app;
--- server-local.js
+++ server-local.js
@@ -1,4 +1,9 @@
-'use strict';
+"use strict";
+//Node.js
+const dotenv = require("dotenv");
+dotenv.config();
 
-const app = require('./express/server');
-app.listen(3000, () => console.log('Local app listening on port 3000!'));
+console.log("process.env.YQ", process.env.YQTOKEN);
+
+const app = require("./express/server");
+app.listen(3000, () => console.log("Local app listening on port 3000!"));
